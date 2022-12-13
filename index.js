6e1c6d59dedccf1225865c4aaa18bd3fffcfe0c8
davidvveldhuizen <119820695+davidvveldhuizen@users.noreply.github.com>
2022-12-13 22:53:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const mainContainer = document.querySelector(".mainContainer");
+const resultsContainer = document.querySelector(".resultsContainer");
+
 const fooiInput = document.querySelector(".fooiInput");
 
 const addMember = document.querySelector(".addMember");
-
 const clearAll = document.querySelector(".clearAll");
+const back = document.querySelector(".backButton");
 
 const membersContainer = document.querySelector(".members");
 const firstMember = document.getElementById("member1");
@@ -72,29 +75,104 @@ calculate.addEventListener("click", ()=>{
   let uren = [];
   let totaleUren = 0;
   let fooi = [];
-  let totaleFooi = fooiInput.valueAsNumber;
+  let totaleFooi;
   let fooiPerUur = 0;
 
-  members.forEach(member => {
-    names.push(member.querySelector(".naamInput").value);
+  let acces = true;
+  
+  if(fooiInput.value === ""){
+    acces = false;
+    fooiInput.style.backgroundColor = "red";
+  }else{
+    fooiInput.style.backgroundColor = "rgb(241, 218, 141)";
+    totaleFooi = fooiInput.valueAsNumber;
+  }
+
+  members.forEach(member => { // get data from each member
+    const currMember = membersContainer.querySelector(`#${member.id}`);
+    currNaam = member.querySelector(".naamInput").value;
+    if(currNaam === ""){
+      const m = currMember.querySelector(".naamInput");
+      m.style.backgroundColor = "red";
+      acces = false;
+    }else{
+      const m = currMember.querySelector(".naamInput");
+      m.style.backgroundColor = "rgb(144, 175, 204)";
+
+      names.push(currNaam);
+    }
+
     const d = member.querySelector(".urenInput").valueAsDate;
-    const us = d.getHours()-1;
-    const ms = d.getMinutes();
-    const u = us+(ms/60);
-    uren.push(u);
+    if(d === null){
+      const ureninperror = currMember.querySelector(".urenInput");
+      ureninperror.style.backgroundColor = "red";
+      acces = false;
+    }else{
+      const ureninperror = currMember.querySelector(".urenInput");
+      ureninperror.style.backgroundColor = "rgb(144, 175, 204)";
+      const us = d.getHours()-1;
+      const ms = d.getMinutes();
+      const u = us+(ms/60);
+      uren.push(u);
+    }
   });
 
-  uren.forEach(i =>{
-    totaleUren +=i;
-  });
+  if(acces){ // checks for acces
+    uren.forEach(i =>{ // counts up all the hours
+      totaleUren +=i;
+    });
 
-  fooiPerUur = totaleFooi/totaleUren;
-  console.log(`totale uren: ${totaleUren} totale fooi: ${totaleFooi}`);
-  console.log(`fooi per uur = ${fooiPerUur}`);
+    fooiPerUur = totaleFooi/totaleUren; // calculates the fooi per hour
+  
+    fooi = calculateFooi(uren, fooiPerUur);
 
-  for(let i = 0; i<names.length;i++){
-    let f = uren[i] * fooiPerUur;
-    fooi.push(f);
-    console.log(`${names[i]} heeft ${uren[i]} gewerk en verdient ${f} euro`);
+    moveToResults(names, uren, fooi);
   }
 })
+
+function calculateFooi(uren, fooiPerUur){
+  const innerFooi = [];
+  let change = 0;
+  for(let i = 0; i<uren.length;i++){ // gives each member its equivelant fooi
+    let f = uren[i] * fooiPerUur;
+    f = Math.floor(f*20)/20;
+    innerFooi.push(f);
+    change += (uren[i]*fooiPerUur)-f;
+  }
+  console.log(change);
+  change = Math.round(change*100)/100;
+  console.log(change);
+  return innerFooi;
+}
+
+function moveToResults(names, uren, fooi){
+  mainContainer.style.display = "none"; // switch from the calculator to the results
+  resultsContainer.style.display = "flex";
+
+  const results = resultsContainer.querySelector(".results");
+  const example = results.querySelector(".example");
+  for(let i = 0; i<names.length;i++){
+    const newText = example.cloneNode(true); //clone the example
+    newText.classList.remove("example"); //change the class name of the example
+    newText.classList.add("resultsText");
+    newText.id = `${i}`;
+
+    newText.querySelector(".naamHere").innerHTML = `${names[i]} `;
+    newText.querySelector(".urenHere").innerHTML = `${Math.floor(uren[i]*100)/100} `;
+    newText.querySelector(".fooiHere").innerHTML = `${fooi[i]} `;
+
+    results.appendChild(newText);
+  }
+}
+
+back.addEventListener('click', ()=>{
+  const results = resultsContainer.querySelector(".results");
+  const example = results.querySelector(".example");
+  console.log(results.children);
+  results.innerHTML = "";
+  results.append(example);
+  console.log(results.children);
+  resultsContainer.style.display = "none";
+  mainContainer.style.display = "flex";
+})
+
